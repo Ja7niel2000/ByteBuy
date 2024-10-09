5b9bcb6c37e028fa4854c8b08721375e57eae0fb
ja7niel2000 <jatniel2000@ciencias.unam.mx>
2024-10-09 13:46:56 -0600

    listo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,34 @@
 import { Routes } from '@angular/router';
-import { CategoryComponent } from './modules/product/component/category/category.component';
 
-export const routes: Routes = [{path:"categoria", component:CategoryComponent}];
+import { CategoryComponent } from './modules/product/component/category/category.component';
+import { MainComponent } from './modules/layout/component/main/main.component';
+import { LoginComponent } from './modules/auth/component/login/login.component';
+import { RegisterComponent } from './modules/auth/component/register/register.component';
+import { SecuredComponent } from './modules/auth/component/secured/secured.component';
+import { authenticationGuard } from './modules/auth/authentication.guard';
+
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/region',
+        pathMatch: 'full'
+    },
+    {
+        path: 'categoria',
+        component: CategoryComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
+    },
+    {
+        path: 'secured',
+        component: SecuredComponent, 
+        canActivate: [authenticationGuard]
+    }
+
+];
